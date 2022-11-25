2238b6d042f1c0ecde55dd314f4cde18c1292c96
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-11-25 20:02:00 -0300

    Criando arquivos para tratar erros globalmente e tirando o try/catch

--- backend/src/controllers/AccountController.ts
+++ backend/src/controllers/AccountController.ts
@@ -1,52 +1,48 @@
 import { Request, Response } from 'express';
 import { accountRepository } from '../repositories/accountRepository';
 import bcrypt from 'bcrypt';
+import { BadRequestError } from '../helpers/api-errors';
 
 export class AccountController {
     async create(req: Request, res: Response) {
         const { nome, cpf, data_nascimento, telefone, email, senha_app, senha_transacao } = req.body;
 
-        try {
-            const account = await accountRepository.findOneBy({ cpf });
+        const account = await accountRepository.findOneBy({ cpf });
 
-            if (account) {
-                return res.status(404).json({mensagem: "Já existe uma conta aberta com esse CPF."});
-            }
-
-            const encryptedPasswordApp = await bcrypt.hash(senha_app, 10);
-
-            const encryptedPasswordTransactions = await bcrypt.hash(senha_transacao, 10);
-
-            const newAccount = accountRepository.create({
-                nome,
-                saldo: 0,
-                cpf,
-                data_nascimento,
-                telefone,
-                email,
-                senha_app: encryptedPasswordApp,
-                senha_transacao: encryptedPasswordTransactions
-            });
-
-           const accountData = await accountRepository.save(newAccount);
-
-            const userData = {
-                id: accountData.id,
-                nome: accountData.nome,
-                saldo: accountData.saldo,
-                usuario: {
-                    cpf: accountData.cpf,
-                    data_nascimento: accountData.data_nascimento,
-                    telefone: accountData.telefone,
-                    email: accountData.email
-                }
-            }
-
-            return res.status(201).json(userData);
-
-        } catch (error) {
-            console.log(error)
-            return res.status(500).json({ mensagem: "Erro interno do servidor." });
+        if (account) {
+            throw new BadRequestError("Já existe uma conta aberta com esse CPF.");
         }
+
+        const encryptedPasswordApp = await bcrypt.hash(senha_app, 10);
+
+        const encryptedPasswordTransactions = await bcrypt.hash(senha_transacao, 10);
+
+        const newAccount = accountRepository.create({
+            nome,
+            saldo: 0,
+            cpf,
+            data_nascimento,
+            telefone,
+            email,
+            senha_app: encryptedPasswordApp,
+            senha_transacao: encryptedPasswordTransactions
+        });
+
+        const accountData = await accountRepository.save(newAccount);
+
+        const userData = {
+            id: accountData.id,
+            nome: accountData.nome,
+            saldo: accountData.saldo,
+            usuario: {
+                cpf: accountData.cpf,
+                data_nascimento: accountData.data_nascimento,
+                telefone: accountData.telefone,
+                email: accountData.email
+            }
+        }
+
+        return res.status(201).json(userData);
+
     }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
+import 'express-async-errors';
 import express from 'express';
 import { AppDataSource } from './data-source';
 import routes from './routes/routes';
 import cors from 'cors';
+import { errorMiddleware } from './middlewares/error';
 
 AppDataSource.initialize().then(() => {
     const app = express();
@@ -11,6 +13,8 @@ AppDataSource.initialize().then(() => {
     app.use(express.json());
 
     app.use(routes);
+
+    app.use(errorMiddleware);
     
     return app.listen(process.env.PORT);
 })
